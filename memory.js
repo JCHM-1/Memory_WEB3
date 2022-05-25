0cf93cb86aed5deb372639e8a856696b81e20dfe
Dorienk97 <dorienkuperus@gmail.com>
2022-05-25 11:39:04 +0200

    Update

--- memory.js
+++ memory.js
@@ -22,10 +22,10 @@ document.querySelectorAll(".kaart").forEach(n => n.addEventListener("click", () 
     } 
 }))
 
-function darkMode() {
-    var elemment = document.body;
-    elemment.classList.toggle("dark-mode")
-}
+// function darkMode() {
+//     var elemment = document.body;
+//     elemment.classList.toggle("dark-mode")
+// }
 
 
 
@@ -39,21 +39,21 @@ function darkMode() {
 
 // document.querySelector("")
 
-function genDivs (rows, cols) {
-    var e = document.getElementById("game");
-    for (var r = 0; r < rows; r++) {
-      var row = document.createElement("div");
-      row.className = "row";
-      for (var c = 0; c < cols; c++) {
-        var cell = document.createElement("div");
-        if (r == 10 && c == 20)
-          cell.className = "gridsquare begin";
-        else if (r == 10 && c == 40)
-          cell.className = "gridsquare end";
-        else
-          cell.className = "gridsquare";
-        row.appendChild(cell);
-      }
-      e.appendChild(row);
-    }
-  }
+// function genDivs (rows, cols) {
+//     var e = document.getElementById("game");
+//     for (var r = 0; r < rows; r++) {
+//       var row = document.createElement("div");
+//       row.className = "row";
+//       for (var c = 0; c < cols; c++) {
+//         var cell = document.createElement("div");
+//         if (r == 10 && c == 20)
+//           cell.className = "gridsquare begin";
+//         else if (r == 10 && c == 40)
+//           cell.className = "gridsquare end";
+//         else
+//           cell.className = "gridsquare";
+//         row.appendChild(cell);
+//       }
+//       e.appendChild(row);
+//     }
+//   }
