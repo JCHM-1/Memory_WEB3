843711cc42ff9af2b53a8a9b6249d3279281c581
Dorienk97 <dorienkuperus@gmail.com>
2022-06-16 18:36:50 +0200

    upd POST /register

--- login.js
+++ login.js
@@ -1,53 +1,110 @@
-//const auth = new Auth();
+// Moet in memory.html:
+// const auth = new Auth();
 
-class createPost {
+class Login {
     constructor(form, fields, signup) {
         this.form = form;
         this.fields = fields;
-        this.createJson();
+        this.signup = signup;
+        this.validateonSubmit();
     }
 
-    createJson(){
-        let self = this
-        let object = {}
+    validateonSubmit() {
+
+        let self = this;
+        let data = {}
 
         this.form.addEventListener("submit", (e) => {
             console.log("In submit")
             e.preventDefault();
             var error = 0;
-
             self.fields.forEach((field) => {
-                object[field] = document.getElementById(field).value;
+                console.log(field)
+                const input = document.getElementById(field);
+
+                data[field] = input.value
+
+                if (self.validateFields(input) == false) {
+                    error++;
+                }
+
             });
+            if (error == 0) {
+                //do login api here
+                // localStorage.setItem("auth", 1);
+                //
+                // this.form.submit();
 
-            console.log(object)
-        })
+                console.log("final data", data)
 
-        this.sendData(object, signup)
+                if (this.signup) {
+                    fetch("http://localhost:8000/register", {
+                        method: "POST",
+                        body: JSON.stringify(data),
+                        header: {
+                            "content-tyoe": "application/json; charset=UTF-8",
+                        },
+                    })
+                        .then(data => {
+                            console.log(data.statusText)
+                        })
+                        .catch(data => {
+                            console.error("Error: ", data.message)
+                        })
+                } else {
+
+                }
+
+            }
+        });
     }
 
-    sendData(data, signup){
-        const XHR = new XMLHttpRequest();
-
-        // Define what happens on successful data submission
-        XHR.addEventListener( "load", function(event) {
-            console.log( event.target.responseText );
-        } );
-
-        // Define what happens in case of error
-        XHR.addEventListener( "error", function( event ) {
-            console.log( 'Oops! Something went wrong.' );
-        } );
-
-        if(signup){
-            XHR.open( "POST", "http://localhost:8000/register" );
+    validateFields(field) {
+        if (field.value.trim() === "") {
+            this.setStatus(
+                field,
+                `${field.nextSibling.innerText} cannot be blank`,
+                "error"
+            );
+            return false;
         } else {
-            XHR.open( "POST", "/api/login_check" );
+            if (field.type == "password") {
+                console.log(field)
+                if (field.value.length < 8) {
+                    console.log(field.nextElementSibling)
+                    this.setStatus(
+                        field,
+                        `${field.previousElementSibling.innerText} must be at least 8 characters`,
+                        "error"
+                    );
+                    return false;
+                } else {
+                    this.setStatus(field, null, "success");
+                    return true;
+                }
+            } else {
+                this.setStatus(field, null, "success");
+                return true;
+            }
         }
-        XHR.send(data)
     }
 
+    setStatus(field, message, status) {
+        console.log(field)
+        const errorMessage = field.nextElementSibling
 
+        if (status == "success") {
+            if (errorMessage) {
+                errorMessage.innerText = "";
+            }
+            field.classList.remove("input-error");
+        }
+
+        if (status == "error") {
+            errorMessage.innerText = message;
+
+        }
+    }
 }
 
 // Access the form element...
@@ -56,10 +113,10 @@ const loginForm = document.getElementById("login")
 
 if (signupForm) {
     const fields = ["username", "email", "password"];
-    const validator = new createPost(signupForm, fields, 1);
+    const validator = new Login(signupForm, fields, 1);
 } else{
     const fields = ["username", "password"];
-    const validator = new createPost(loginForm, fields, 0);
+    const validator = new Login(loginForm, fields, 0);
 }
 
 
