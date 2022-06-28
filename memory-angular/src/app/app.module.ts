52560d383eb22912c6004350d9a95b631435103f
Dorienk97 <dorienkuperus@gmail.com>
2022-06-28 18:00:02 +0200

    upd aggr

--- memory-angular/src/app/app.module.ts
+++ memory-angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AggregateComponent } from './components/aggregate/aggregate.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AggregateComponent
+    AggregateComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
