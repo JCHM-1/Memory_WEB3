3f0b8ee12491b256bb1553670519cf9484050831
Dorienk97 <dorienkuperus@gmail.com>
2022-06-29 03:07:30 +0200

    upd smol visual

--- memory-angular/src/app/components/home/home.component.ts
+++ memory-angular/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient} from "@angular/common/http";
+import {tap} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -25,20 +26,26 @@ export class HomeComponent implements OnInit {
   }
 
   getAggregateList() {
-    this.http.get<any>('http://localhost:8000/api/admin/aggregate', {
+    return this.http.get<any>('http://localhost:8000/api/admin/aggregate', {
       headers: {
         'Authorization': `bearer ${this.token}`,
         'Content-Type': 'application/json',
         'Accept': 'application/json'
       }
-    }).subscribe(
-      response => {
-        this.aantSpellen = response[0]['aantal_spellen']
-        this.aantSpelers = response[1]['aantal_spelers']
-        console.log(this.aantSpellen)
-        if(response.status == 200){
-          console.log(response)
+    }).pipe(
+      tap( // Log the result or error
+        {
+          next: (data) => data,
+          error: () => {
+            // refer to error page
+            window.alert("Je bent geen admin")
+            window.close()
+          }
         }
-      });
+      )
+    ).subscribe(response => {
+      this.aantSpellen = response[0]['aantal_spellen']
+      this.aantSpelers = response[1]['aantal_spelers']
+    })
   }
 }
