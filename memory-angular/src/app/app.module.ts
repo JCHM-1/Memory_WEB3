6aaebb4f0a3e3c4a0c465675387d22712600f502
JCHM-1 <j.c.huizinga.mora@st.hanze.nl>
2022-06-29 15:02:51 +0200

    hopelijk goed

--- memory-angular/src/app/app.module.ts
+++ memory-angular/src/app/app.module.ts
@@ -2,22 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
 import { AggregateComponent } from './components/aggregate/aggregate.component';
-import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from "@angular/common/http";
 import {HttpService} from "./services/http.service";
+import { PlayersComponent } from './components/players/players.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     AggregateComponent,
-    HomeComponent
+    PlayersComponent,
+    HeaderComponent
   ],
   imports: [
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]
