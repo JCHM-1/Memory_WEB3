d765105e168f8d10afbf2131407f25a5455ebf25
Dorienk97 <dorienkuperus@gmail.com>
2022-07-03 15:15:45 +0200

    upd aggr werkt

--- memory-angular/src/app/components/aggregate/aggregate.component.ts
+++ memory-angular/src/app/components/aggregate/aggregate.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import { single } from '../aggregate/data';
+import {HttpClient} from "@angular/common/http";
+import {TokenStorageService} from "../../services/token-storage.service";
 
 export interface PeriodicElement {
   name: string;
@@ -28,28 +30,75 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: './aggregate.component.html',
   styleUrls: ['./aggregate.component.scss']
 })
-  
+
 export class AggregateComponent {
 	chartOptions = {
 	  animationEnabled: true,
 	  theme: "dark2",
 	  exportEnabled: true,
 	  title: {
-		text: "Developer Work Week"
+		text: "Api\'s"
 	  },
 	  subtitles: [{
-		text: "Median hours/week"
+		text: "Percentage of players"
 	  }],
 	  data: [{
 		type: "pie", //change type to column, line, area, doughnut, etc
 		indexLabel: "{name}: {y}%",
 		dataPoints: [
-			{ name: "Overhead", y: 9.1 },
-			{ name: "Problem Solving", y: 3.7 },
-			{ name: "Debugging", y: 36.4 },
-			{ name: "Writing Code", y: 30.7 },
-			{ name: "Firefighting", y: 20.1 }
+			{ name: "No api", y: 9.1 },
+			{ name: "cats", y: 3.7 },
+			{ name: "clouds", y: 36.4 },
+			{ name: "dogs", y: 30.7 },
+			{ name: "people", y: 20.1 }
 		]
 	  }]
   }
-}  
+
+  public isDataAvailable:boolean=false;
+  token: string | null;
+  aantSpellen: number;
+  aantSpelers: number;
+
+  constructor(private http: HttpClient, private tokenStorage: TokenStorageService) {
+    this.token = ""
+    this.aantSpellen = 0
+    this.aantSpelers = 0
+  }
+
+
+  ngOnInit(): void {
+    this.token =this.tokenStorage.getToken() as string
+    console.log(this.token)
+    this.getAggregateList()
+
+  }
+
+  getAggregateList() {
+    this.http.get<any>('http://localhost:8000/api/admin/aggregate', {
+      headers: {
+        'Authorization': `bearer ${this.token}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }).subscribe(
+      response => {
+        this.aantSpellen = response[0]['aantal_spellen']
+        this.aantSpelers = response[1]['aantal_spelers']
+        console.log(response[2])
+        console.log(response[2][0])
+        console.log(this.chartOptions.data[0].dataPoints)
+        for(let i=0; i<response[2].length; i++){
+          console.log(response[2][i].aantal); //use i instead of 0
+          this.chartOptions.data[0].dataPoints[i].y = response[2][i].aantal
+        }
+        this.isDataAvailable=true;
+        console.log("na einde: ", this.chartOptions.data[0].dataPoints)
+
+        if(response.status == 200){
+          console.log(response)
+        }
+      });
+  }
+
+}
